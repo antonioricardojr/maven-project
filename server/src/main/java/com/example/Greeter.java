362bcb5d4ba2fec8fd4fb385378faa21095ff014
Antonio Marques Jr <antonioricardojr@gmail.com>
2017-05-10 00:31:37 -0300

    adding commit

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,7 @@ public class Greeter {
 
   /**
   * @param someone of the name of a person
+  * @return greeting
   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
